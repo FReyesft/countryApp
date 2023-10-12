a359388cd3b60cb188e59cfc27e5c4730d43d89c
Fernando Reyez Pinto <f.reyesft@gmail.com>
2023-10-11 20:35:26 -0500

    new component byCapital

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomePageComponent } from "./shared/pages/homePage/homePage.component";
+import { AboutPageComponent } from "./shared/pages/aboutPage/aboutPage.component";
+import { ContactPageComponent } from './shared/pages/contactPage/contactPage.component'
+
+
+const routes: Routes = [
+	{ path: 'home', component: HomePageComponent },
+	{ path: 'about', component: AboutPageComponent },
+	{ path: 'contact', component: ContactPageComponent },
+	{ path: '**', redirectTo: 'home' }
+]
+@NgModule({
+	imports: [
+		RouterModule.forRoot(routes),
+	],
+	exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ src/app/shared/pages/contactPage/contactPage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contactPage',
+  templateUrl: './contactPage.component.html',
+  styleUrls: ['./contactPage.component.css']
+})
+export class ContactPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
