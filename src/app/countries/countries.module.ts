403645ca8d6db182156b5967c6f9eff0ae05e556
Fernando Reyez Pinto <f.reyesft@gmail.com>
2023-10-12 19:01:57 -0500

    add new components

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryOPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 
 
 
 @NgModule({
   declarations: [
-    ByCapitalPageComponent
+    ByCapitalPageComponent,
+    ByCountryOPageComponent,
+    ByRegionPageComponent
   ],
   imports: [
     CommonModule
